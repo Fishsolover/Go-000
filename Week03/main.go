2ebc4ce9ca6d6f8624143aa0a53e20268d90197f
lvs <690718404@qq.com>
2020-12-09 19:57:18 +0800

    Week03作业

--- Week03/main.go
+++ Week03/main.go
@@ -6,6 +6,9 @@ import (
 	"fmt"
 	"golang.org/x/sync/errgroup"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 var CloseChan chan bool
@@ -17,6 +20,7 @@ func startHttpServer() error {
 	})
 
 	http.HandleFunc("/close", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Printf("开始关闭端口")
 		CloseChan <- true
 	})
 
@@ -26,15 +30,49 @@ func startHttpServer() error {
 
 //关闭一个HTTP服务
 func closeHttpServer() error {
-	if <-CloseChan {
+	select {
+	case <-CloseChan:
 		return errors.New("退出")
+	}
+}
+
+type Signal struct {
+	SignalChan chan os.Signal
+	SignalDone chan bool
+	Actions    []SignalAction
+}
+
+type SignalAction interface {
+	Name() string
+	Action(os.Signal) error
+}
+
+//监听
+func listenSignals() error {
+	var signalMain Signal
+	signalMain.SignalChan = make(chan os.Signal, 1)
+	signalMain.SignalDone = make(chan bool, 1)
+	signalMain.Actions = registerActions()
+	signal.Notify(signalMain.SignalChan, syscall.SIGINT)
+	var sg os.Signal
+	select {
+	case sg = <-signalMain.SignalChan:
+		return errors.New(sg.String())
 	}
 	return nil
 }
 
-//监听
+//注册动作
+
+func registerActions() []SignalAction {
+	var signalActions []SignalAction
+	//此处开启注册
+	return signalActions
+}
 
 func main() {
+
+	CloseChan = make(chan bool, 1)
 
 	group, _ := errgroup.WithContext(context.Background())
 
@@ -46,8 +84,14 @@ func main() {
 		return closeHttpServer()
 	})
 
+	group.Go(func() error {
+		return listenSignals()
+	})
+
 	if err := group.Wait(); err != nil {
 		panic(err.Error())
+	} else {
+		panic("")
 	}
 
 }
